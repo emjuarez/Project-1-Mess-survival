39529a4c0e5cf617e603d4da8828b7b3d3cbe277
Emanueljuarez <zerauleuname@gmail.com>
2022-01-24 20:08:36 -0600

    draw de clases e instancias

--- javascript/mess.js
+++ javascript/mess.js
@@ -12,9 +12,9 @@ class Piso{
         this.x = 0;
         this.y = 500;
         this.width = canvas.width;
-        this.heigth = 700;
+        this.height = 700;
         this.image = new Image();
-        this.image.src = "../assets/images/piso.png"
+        this.image.src = "assets/images/piso.png"
     }
 
     // metodos
@@ -30,7 +30,7 @@ class City{
         this.x = 0;
         this.y = 0;
         this.width = canvas.width;
-        this.heigth = 1046;
+        this.height = 1046;
         this.image = new Image();
         this.image.src = "../assets/images/fondo.png"
     }
@@ -45,7 +45,7 @@ class Lineas {
         this.x = 0;
         this.y = 500;
         this.width = canvas.width;
-        this.heigth = 700;
+        this.height = 700;
         this.image = new Image();
         this.image.src = "../assets/images/lineas.png"
     }
@@ -55,22 +55,69 @@ class Lineas {
     }
 }
 
+// class Bike {
+//     constructor(){
+//         this.x = 310;
+//         this.y = 1070;
+//         this.width = 180;
+//         this.height = 135;
+//         this.image = new Image();
+//         this.image.src = "../assets/images/charone.png"
+//     }
+//     // Metodos
+//     draw(){
+//         ctx.drawImage(this.image, this.x, this.y, this.width, this.height) 
+//     }
+// }
 
+class Bike {
+    constructor(x,y,w,h){
+        this.x= x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.image = new Image();
+        this.image.src = "../assets/images/charone.png"
+    }
 
+    //Metodos
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+}
+
+class Enemies {
+    constructor(){
+
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height) 
+    }
+}
 
 
 // seccion de instancias
 
 const piso = new Piso()
 const city = new City()
-const lines = new Lineas
+const lines = new Lineas()
+const bike = new Bike(340, 1011, 135,180)
+
+
+// funciones
 
 function updateCanvas() {
     frames++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     city.draw()
     piso.draw()
-    if(requestId){
-      requestId = requestAnimationFrame(updateCanvas)
-    }   
+    lines.draw()
+    bike.draw()
+    // if(requestId){
+    //   requestId = requestAnimationFrame(updateCanvas)
+    // }   
+    requestAnimationFrame(updateCanvas)
   }
+
+  updateCanvas()
+
