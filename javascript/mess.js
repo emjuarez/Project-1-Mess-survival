4d0536080dd4a8a7681e7583355fc559aee59533
Emanueljuarez <zerauleuname@gmail.com>
2022-01-25 13:41:07 -0600

    funcion drawObstacles y generateObstacles implementadas

--- javascript/mess.js
+++ javascript/mess.js
@@ -3,6 +3,13 @@ const ctx = canvas.getContext("2d");
 
 let frames = 0;
 
+const imageObstacles = [
+  "/assets/images/carone.png",
+  "/assets/images/cartwo.png",
+  "/assets/images/hole.png",
+]
+const obstacles=[]
+
 let requestId;
 
 
@@ -112,17 +119,26 @@ class Bike {
 }
 
 class Obstacles {
-  constructor(x,y,w,h){
+  constructor(x,y,w,h,img){
     this.x = x;
     this.y = y;
     this.width = w;
     this.height = h;
     this.image = new Image();
-    this.image.src = "../assets/images/carone.png"
+    this.image.src = img;
   }
    // metodos
   draw(){
+    this.y += 2
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+  }
+  collision(item){
+    return(
+      this.x < item.x + item.width &&
+      this.x + this.width > item.x &&
+      this.y < item.y + item.height &&
+      this.y + this.height > item.y
+    )
   }
 }
 
@@ -135,21 +151,44 @@ const piso = new Piso()
 const city = new City()
 const lines = new Lineas()
 const bike = new Bike(340, 1011, 135,180)
-const obstacle = new Obstacles(340, 1011, 135,180)
+// const obstacle = new Obstacles(340, 1011, 135,180)
 // const house = new House()
 
 
 // funciones
 
+function generateObstacles(){
+  if (frames % 200 === 0){
+    let x = Math.floor(Math.random() * (740 - 140) + 10);
+    let imgRand = Math.floor(Math.random() * imageObstacles.length);
+    const obstacle = new Obstacles(x, 400,150,150 ,imageObstacles[imgRand]);
+    obstacles.push(obstacle);
+  }
+}
+
+// x,y,w,h,img
+
+function drawObstacles(){
+  obstacles.forEach((obstacle, index_obstacle) =>{
+    obstacle.draw();
+    if(bike.collision(obstacle)){
+      console.log("ouch");
+      requestID = undefined;
+      // fondo.gameOver();
+    }
+  })
+}
 
 function updateCanvas() {
     frames++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     piso.draw()
     lines.draw()
+    generateObstacles();
+    drawObstacles();
     city.draw()
     bike.draw()
-    obstacle.draw()
+    // obstacle.draw()
     if(requestId){
       requestId = requestAnimationFrame(updateCanvas)
     }   
